032af8a3052516d7ece37d16b78cf61ee24488e2
Anzai <55052716+owari-hash@users.noreply.github.com>
2025-07-02 00:28:20 +0800

    [UPDATE]

--- src/config-global.ts
+++ src/config-global.ts
@@ -2,4 +2,4 @@
 
 export const GOOGLE_MAP_API = process.env.NEXT_PUBLIC_MAP_API;
 
-export const DJANGO_API_URL = process.env.NEXT_PUBLIC_DJANGO_API_URL || 'http://127.0.0.1:8000';
+export const DJANGO_API_URL = process.env.NEXT_PUBLIC_DJANGO_API_URL || 'http://13.237.58.62/';
